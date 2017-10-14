4aa05dae75b91bf2af17bea061cd2d564bc3a9a2
JosemaGonzalez <josemagp94@gmail.com>
2017-10-14 21:13:04 +0200

    14102017
    app\Resources\views\base2.html.twig
    -añado plugins para las busquedas
    app\Resources\views\convivencia\diarioAulaConvivencia\diarioAula.html.twig
    -rediseño completo
    web\css\style.css
    -quito bordes para que sea uniforme
    web\js\chosen\chosen.jquery.min.js
    -cambio mensaje por defecto al borrar rango de horas (ojo al actualizar el plugin)
    web\js\script.js
    -añado estilo al buscador

--- web/js/script.js
+++ web/js/script.js
@@ -2,12 +2,25 @@
  * Created by Josema on 04/10/2017.
  */
 $(document).ready(function () {
+
     $(".dropdown-button").dropdown({ hover: true });
     let drop = $(".dropdown-button").width();
     $(".dropdown-content").css("margin-left",drop);
     $('.button-collapse').sideNav();
     $(".modal").modal();
     $( window ).on("load resize",function() {
+        //buscador en diario
+        $("#horasSelect_chosen").css("min-width","100");
+        $("#busq .row").css("margin-bottom","0");
+        $("#horasSelect_chosen").css("width","110%");
+        $("#horasSelect_chosen .chosen-choices").css("min-height","50");
+        $("#horasSelect_chosen  .chosen-choices").css("background-color","none");
+        $("#horasSelect_chosen  .chosen-choices").css("background-image","none");
+        $("#horasSelect_chosen .chosen-choices").css("border","0");
+        if ($(window).width() < "600") {
+            $("#botonbus button").css("margin-top","0");
+        }
+        //fin buscador
         let $nav = $(".navbar-fixed");
         $('.navbar-fixed img').height($nav.height());
         $('.navaltu').height($('.nav-wrapper').height());
