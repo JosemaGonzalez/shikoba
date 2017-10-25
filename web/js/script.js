51084e8fb1dc00bfb89b808b4469199278ab95bb
JosemaGonzalez <josemagp94@gmail.com>
2017-10-25 20:51:21 +0200

    25102017
    app\Resources\views\convivencia\sanciones\sancionesForm.html.twig
    -terminado el diseño
    web\js\convivencia.js
    -se pasa el script que sirve al otro, este se borrará cuando se termine el diseño
    web\js\script.js
    -corregido varios problemas del diseño y tenga buena composicion
    -se quedaban las cajas al ocultar las horas ac, corregido para que se borren siempre que se oculte

--- web/js/script.js
+++ web/js/script.js
@@ -17,11 +17,17 @@ $(document).ready(function () {
     $('#appbundle_diarioaulaconvivencia label').css("color", "white");
 
     $(window).on("load resize", function () {
+        //oculto un icono que no tiene porque estar ahi
+        $('.caret').css("display", "none");
         //estilo material para un selector en sanciones form
         $('#sancion_form_idTipo').material_select();
-        $('.caret').css("display","none");
+        //cajas finales despues del select que van todas
+        // juntas y hay que darle estilos y clases
+        $('#sancion_form').attr("style", "margin-top: 7px;");
         $('#sancion_form > div').addClass("input-field");
-        $('#sancion_form_Observaciones').attr("data-length","200");
+        $('#sancion_form').addClass("col s12 m6");
+        //caja de observaciones
+        $('#sancion_form_Observaciones').attr("data-length", "200");
         $('#sancion_form_Observaciones').characterCounter();
         //estilo al paginador de datatable
         $("#DataTables_Table_0_paginate").addClass("col s12 l8 right");
@@ -86,4 +92,275 @@ $(document).ready(function () {
     $('#usuario_form ul>li').remove();
 
 
+    $('#botonImportar').on('click', function () {
+        $('#contenedorUpload .loader').css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 500)
+    })
+
+
+    // DataTables
+
+    jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+        "date-eu-pre": function (date) {
+            date = date.replace(" ", "");
+
+            if (!date) {
+                return 0;
+            }
+
+            var year;
+            var eu_date = date.split(/[\.\-\/]/);
+
+            /*year (optional)*/
+            if (eu_date[2]) {
+                year = eu_date[2];
+            }
+            else {
+                year = 0;
+            }
+
+            /*month*/
+            var month = eu_date[1];
+            if (month.length == 1) {
+                month = 0 + month;
+            }
+
+            /*day*/
+            var day = eu_date[0];
+            if (day.length == 1) {
+                day = 0 + day;
+            }
+
+            return (year + month + day) * 1;
+        },
+
+        "date-eu-asc": function (a, b) {
+            return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+        },
+
+        "date-eu-desc": function (a, b) {
+            return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+        }
+    });
+
+    $('.tableData').DataTable({
+        "drawCallback": function (settings) {
+            $(".paginate_button.current").addClass("btn  blue darken-4 waves-effect waves-light z-depth-3 pagi");
+            $(".paginate_button").addClass("btn  blue darken-2 waves-effect waves-light z-depth-3 pagi");
+            $(".paginate_button.current").removeClass("paginate_button current");
+            $(".paginate_button").removeClass("paginate_button");
+        },
+        "lengthMenu": [[5, 10, 20, 30, -1], [5, 10, 20, 30, "Todos"]],
+        language: {
+            search: "Busca en la tabla ",
+            paginate: {
+                first: "Primero",
+                previous: "<i class='material-icons pagi'>chevron_left</i>",
+                next: "<i class='material-icons pagi'>chevron_right</i>",
+                last: "Último"
+            },
+            lengthMenu: "Mostrar Resultados _MENU_",
+            emptyTable: "No hay registros en la tabla",
+            info: "Mostrando _END_ registros de _TOTAL_ en total",
+            infoEmpty: "No hay resultados",
+            infoFiltered: "(filtrado de _MAX_ en total)",
+            zeroRecords: "No se encuentra ningun registro",
+        },
+        "aoColumnDefs": [
+            {"sType": "date-eu", "aTargets": [1]}
+        ],
+    });
+
+    $('.tableDataCarnets').DataTable({
+        "drawCallback": function (settings) {
+            $(".paginate_button.current").addClass("btn blue darken-3 waves-effect waves-light z-depth-3");
+            $(".paginate_button").addClass("btn blue darken-3 waves-effect waves-light z-depth-3");
+            $(".paginate_button.current").removeClass("paginate_button current");
+            $(".paginate_button").removeClass("paginate_button");
+        },
+        "lengthMenu": [[5, 10, 20, 30, -1], [5, 10, 20, 30, "Todos"]],
+        language: {
+            search: "Busca en la tabla ",
+            paginate: {
+                first: "Primero",
+                previous: "<i class='material-icons pagi'>chevron_left</i>",
+                next: "<i class='material-icons pagi'>chevron_right</i>",
+                last: "Último"
+            },
+            lengthMenu: "Mostrar Resultados _MENU_",
+            emptyTable: "No hay registros en la tabla",
+            info: "Mostrando _END_ registros de _TOTAL_ en total",
+            infoEmpty: "No hay resultados",
+            infoFiltered: "(filtrado de _MAX_ en total)",
+            zeroRecords: "No se encuentra ningun registro",
+        },
+    }).search(
+        $('.dataTables_filter input').val(),
+        true,
+        false
+    ).draw();
+
+    $('.dataTables_wrapper input').addClass("marginBottom");
+    $('.dataTables_filter').css("text-align", "left");
+
+    // Pantalla Confirmación
+    $('a.confirm').confirm({
+        theme: 'supervan',
+        content: "¿Está seguro de realizar esta acción?",
+        buttons: {
+            Aceptar: function () {
+                location.href = this.$target.attr('href');
+            },
+            Cancelar: function () {
+
+            }
+        },
+        columnClass: 'confirmdialog'
+    });
+
+    $('body').on('click', '.checkAbrir', function () {
+        $.each($('.selectForm input'), function (key, value) {
+            if ($(value).prop('checked') == false) {
+                $(value).next('label').show(250);
+                $(value).show(250);
+            }
+        });
+
+        $('.checkAbrir').removeClass('checkAbrir').addClass('checkCerrar').val('-');
+    })
+        .on('click', '.checkCerrar', function () {
+            $.each($('.selectForm input[type=checkbox]'), function (key, value) {
+                if ($(value).prop('checked') == false) {
+                    $(value).next('label').hide(250);
+                    $(value).hide(250);
+                }
+            });
+
+            $('.checkCerrar').removeClass('checkCerrar').addClass('checkAbrir').val('+');
+        }).on('click', '.checkNewSancion', function () {
+        // Funcionalidad boton añadir Sanción HoraAC
+        $(this).before(contenedorNewHoraSancion());
+    })
+    //PARTE FORM BÚSQUEDA AVANZADA
+        .on('click', '.busquedaMas', function () {
+            $('#cursos').show(300).css('display', 'flex');
+            $(this).removeClass('busquedaMas').addClass('busquedaMenos');
+        })
+        .on('click', '.busquedaMenos', function () {
+            $('#cursos').hide(300);
+            $(this).removeClass('busquedaMenos').addClass('busquedaMas');
+        });
+    const SANCION_TYPE_HORAS = 5;
+    const HORAS_CLASE = {
+        '1': '8:15 - 9:15',
+        '2': '9:15 - 10:15',
+        '3': '10:15 - 11-15',
+        '4': '11:40 - 12:40',
+        '5': '12:40 - 13:40',
+        '6': '13:40 - 14:40'
+    };
+
+
+    //CHOSEN
+    $('.chosen-select').chosen();
+
+    //DATEPICKER
+    $.datepicker.regional['es'] = {
+        closeText: 'Cerrar',
+        prevText: '< Ant',
+        nextText: 'Sig >',
+        currentText: 'Hoy',
+        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Juv', 'Vie', 'Sáb'],
+        dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
+        weekHeader: 'Sm',
+        dateFormat: 'dd/mm/yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['es']);
+    $('.datepicker').datepicker();
+
+
+    //PARTE FORM, CONDUCTAS MOSTRAR Y OCULTAR
+    $.each($('.selectForm input'), function (key, value) {
+        if ($(value).prop('checked') == true) {
+            ocultarCheckbox();
+            return false;
+        }
+    });
+
+    function ocultarCheckbox() {
+        $.each($('.selectForm input'), function (key, value) {
+            if ($(value).prop('checked') == false) {
+                $(value).next('label').hide();
+                $(value).hide();
+            }
+        });
+
+        $('.selectForm')
+            .after('<div id="divAbrir" class="w3-center"><input type="button" class="w3-button w3-margin w3-text-white checkAbrir" value="+"></div>');
+
+    }
+
+
+    // SANCIONES, SI TIPO ES HORAS O JORNADA MOSTRAR NUEVOS INPUTS
+
+    $('#sancion_form_idTipo').on('change', function () {
+        $('#checkEditSancion').hide();
+        if ($(this).val() == SANCION_TYPE_HORAS) {
+            $('.contenedorFlexEdit').css('visibility', 'visible');
+            let buttonAdd = '<div class="col s12 center"><input type="button" class="btn z-depth-3 blue darken-3 waves-effect waves-light checkNewSancion" value="+"></div>';
+            contenedorNewHoraSancion(buttonAdd);
+        } else {
+            $('.contenedorFlex').hide();
+            $('.contenedorFlexEdit').remove();
+        }
+    });
+
+    /**
+     * Función que añade el contenedor HoraAC con los inputs
+     * @param text HTML que se puede añadir al final del contenedor
+     */
+    function contenedorNewHoraSancion(text = '') {
+        let fecha = new Date();
+        var options = {
+            day: "numeric", month: "2-digit", year: "numeric"
+        };
+        fecha = fecha.toLocaleString('es-Es', options);
+        let input = '<div class="row contenedorFlexEdit">' +
+            '<div class="col s6 input-field">' +
+            '<input type="text" class="datepicker" id="dat" placeholder="." name="fechaHora[]" contenteditable="false" value="' + fecha + '">' +
+            '<label for="dat" class="active">Fecha Hora Sanción</label>' +
+            '</div>' +
+            '<div class="col s6 input-field" id="horasAC">' +
+            '<select name="horaAc[]">';
+        for (let key in HORAS_CLASE) {
+            input += '<option value="' + key + '">' + HORAS_CLASE[key] + '</option>';
+        }
+        input += '</select><label>Hora</label>';
+        input += '</div></div>';
+        $('#sancion_form_idTipo').after(input);
+        $('.contenedorFlexEdit')
+            .append(text);
+        $('.datepicker').datepicker();
+        $('#horasAC > select').material_select();
+        $('.caret').css("display", "none");
+    }
+
+//boton en gestion de alumnos y profesores
+    $('#contenedorUpload').on('change', 'input:file', function () {
+        if ($(this).val()) {
+            $('#contenedorUpload input:submit').removeAttr('disabled');
+        }
+    });
+    //ICHECK
+
+    $('.i-checks').iCheck({
+        checkboxClass: 'icheckbox_square-blue checkbus',
+        radioClass: 'iradio_square-blue',
+    });
 });
